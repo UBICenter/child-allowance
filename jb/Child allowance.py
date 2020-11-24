08fcd6ad1f6f8469173cb638d927610e2e40b706
John Walker <67374091+jwal1024@users.noreply.github.com>
2020-11-24 11:36:35 +1100

    Formatting using black 79

--- jb/Child allowance.py
+++ jb/Child allowance.py
@@ -15,48 +15,64 @@ import numpy as np
 import us
 
 # Read in census data and create a copy for manipulation
-raw = pd.read_csv('https://github.com/MaxGhenis/datarepo/raw/master/pppub20.csv.gz')
+raw = pd.read_csv(
+    "https://github.com/MaxGhenis/datarepo/raw/master/pppub20.csv.gz"
+)
 df = raw.copy(deep=True)
 
 # Define child age identifiers
-df['child_6'] = df.A_AGE < 6 
-df['infant'] = df.A_AGE < 1
-df['toddler'] = df.A_AGE.between(1,2)
-df['preschool'] = df.A_AGE.between(3,5)
-df['person'] = 1
+df["child_6"] = df.A_AGE < 6
+df["infant"] = df.A_AGE < 1
+df["toddler"] = df.A_AGE.between(1, 2)
+df["preschool"] = df.A_AGE.between(3, 5)
+df["person"] = 1
 
-# Aggregate to SPM level 
-SPMU_COLS = ['SPM_ID', 'SPM_WEIGHT', 'SPM_TOTVAL', 'SPM_POVTHRESHOLD', 'SPM_CHILDCAREXPNS']
-spmu = pd.DataFrame(df.groupby(SPMU_COLS)[
-    ['child_6', 'infant', 'toddler', 'preschool', 'person']].sum()).reset_index()
+# Aggregate to SPM level
+SPMU_COLS = [
+    "SPM_ID",
+    "SPM_WEIGHT",
+    "SPM_TOTVAL",
+    "SPM_POVTHRESHOLD",
+    "SPM_CHILDCAREXPNS",
+]
+spmu = pd.DataFrame(
+    df.groupby(SPMU_COLS)[
+        ["child_6", "infant", "toddler", "preschool", "person"]
+    ].sum()
+).reset_index()
 
 # Calculate total cost of transfers, weight number of children
-program_cost = mdf.weighted_sum(spmu, 'SPM_CHILDCAREXPNS', 'SPM_WEIGHT')
-df['total_child_6'] = mdf.weighted_sum(spmu, 'child_6', 'SPM_WEIGHT')
+program_cost = mdf.weighted_sum(spmu, "SPM_CHILDCAREXPNS", "SPM_WEIGHT")
+df["total_child_6"] = mdf.weighted_sum(spmu, "child_6", "SPM_WEIGHT")
 
 # Create copies of the dataset in which to simulate the policies and generate dataset flags
 spmu_replace_cost = df.copy(deep=True)
 spmu_flat_transfer = df.copy(deep=True)
 
 # Create simulation flag
-spmu_replace_cost['replace_cost'] = True
-spmu_flat_transfer['replace_cost'] = False
+spmu_replace_cost["replace_cost"] = True
+spmu_flat_transfer["replace_cost"] = False
 
 # Append dataframes
-simdf = pd.concat([spmu_replace_cost,spmu_flat_transfer],ignore_index=True)
+simdf = pd.concat([spmu_replace_cost, spmu_flat_transfer], ignore_index=True)
 
 # Calculate transfer size to individual SPM units
-simdf['flat_transfer'] = program_cost/simdf.total_child_6
+simdf["flat_transfer"] = program_cost / simdf.total_child_6
 
-# Simulate new income given 1. childcare cost subsidy to household equal to childcare expenditure or 2. flat allowance of equal value using replace_cost flag. 
+# Simulate new income given 1. childcare cost subsidy to household equal to childcare expenditure
+# or 2. flat allowance of equal value using replace_cost flag.
 
-simdf.loc[simdf['replace_cost'] == True, 'new_inc'] = simdf.SPM_TOTVAL + simdf.SPM_CHILDCAREXPNS
-simdf.loc[simdf['replace_cost'] == False, 'new_inc'] = simdf.SPM_TOTVAL + simdf.flat_transfer
+simdf.loc[simdf["replace_cost"] == True, "new_inc"] = (
+    simdf.SPM_TOTVAL + simdf.SPM_CHILDCAREXPNS
+)
+simdf.loc[simdf["replace_cost"] == False, "new_inc"] = (
+    simdf.SPM_TOTVAL + simdf.flat_transfer
+)
 
-# Create poverty flags on simulated incomes 
-simdf['poverty_flag'] = simdf.new_inc<simdf.SPM_POVTHRESHOLD
+# Create poverty flags on simulated incomes
+simdf["poverty_flag"] = simdf.new_inc < simdf.SPM_POVTHRESHOLD
 
-# Disaggregate to person level 
+# Disaggregate to person level
 
 
 # Calculate person-level poverty rate by multiplying through by person-level weights
--- jb/data/make_decile_data.py
+++ jb/data/make_decile_data.py
@@ -26,7 +26,9 @@ import us
 
 # Load data from Nate's repo, which pulls from IPUMS.
 
-raw = pd.read_csv('https://github.com/ngpsu22/State_Child_Allowance_Income_Tax/raw/master/cps_00022.csv.gz')
+raw = pd.read_csv(
+    "https://github.com/ngpsu22/State_Child_Allowance_Income_Tax/raw/master/cps_00022.csv.gz"
+)
 
 df = raw.copy(deep=True)
 
@@ -36,31 +38,40 @@ df.taxinc = np.where(df.taxinc == 9999999, 0, df.taxinc)
 df.adjginc = np.where(df.adjginc == 99999999, 0, df.adjginc)
 df.asecwt /= 3  # 3 years
 df.spmwt /= 3
-df['child'] = df.age < 18
-df['person'] = 1
-df['state'] = df.statefip.apply(
-    lambda x: us.states.lookup(str(x).zfill(2)).name).tolist()
+df["child"] = df.age < 18
+df["person"] = 1
+df["state"] = df.statefip.apply(
+    lambda x: us.states.lookup(str(x).zfill(2)).name
+).tolist()
 
 """Aggregate to SPM unit."""
 
-SPMU_COLS = ['spmfamunit', 'year', 'state', 'spmwt', 'spmtotres']
-spmu = pd.DataFrame(df.groupby(SPMU_COLS)[
-    ['child', 'person', 'taxinc']].sum()).reset_index()
+SPMU_COLS = ["spmfamunit", "year", "state", "spmwt", "spmtotres"]
+spmu = pd.DataFrame(
+    df.groupby(SPMU_COLS)[["child", "person", "taxinc"]].sum()
+).reset_index()
 # Calculate weight that represents number of people.
 # Note: no longer used.
-spmu['wt'] = spmu.spmwt * spmu.person
-spmu['spm_resources_pp'] = spmu.spmtotres / spmu.person
+spmu["wt"] = spmu.spmwt * spmu.person
+spmu["spm_resources_pp"] = spmu.spmtotres / spmu.person
 # spmu.columns = ['spm_child', 'spm_person', 'spm_taxinc']
-mdf.add_weighted_quantiles(spmu, 'spm_resources_pp', 'spmwt')
-spmu.drop(['spm_resources_pp_percentile', 'spm_resources_pp_2percentile',
-           'spm_resources_pp_ventile', 'spm_resources_pp_quintile',
-           'spm_resources_pp_quartile'],
-          axis=1, inplace=True)
+mdf.add_weighted_quantiles(spmu, "spm_resources_pp", "spmwt")
+spmu.drop(
+    [
+        "spm_resources_pp_percentile",
+        "spm_resources_pp_2percentile",
+        "spm_resources_pp_ventile",
+        "spm_resources_pp_quintile",
+        "spm_resources_pp_quartile",
+    ],
+    axis=1,
+    inplace=True,
+)
 # Include negatives in the first decile.
 # TODO: Make this an option in microdf.
 spmu.spm_resources_pp_decile = np.maximum(spmu.spm_resources_pp_decile, 1)
 
-spmu.groupby('spm_resources_pp_decile')[['wt', 'spmwt']].sum()
+spmu.groupby("spm_resources_pp_decile")[["wt", "spmwt"]].sum()
 
 """Now for state."""
 
@@ -69,9 +80,9 @@ l = []
 for state in states:
     # Get row and spm_resources_pp_decile_state
     tmp = spmu[spmu.state == state].copy(deep=True)
-    mdf.add_weighted_quantiles(tmp, 'spm_resources_pp', 'spmwt')
+    mdf.add_weighted_quantiles(tmp, "spm_resources_pp", "spmwt")
     l.append(tmp.spm_resources_pp_decile)
-state_decile = pd.concat(l).rename('spm_resources_pp_decile_state')
+state_decile = pd.concat(l).rename("spm_resources_pp_decile_state")
 state_decile = np.maximum(state_decile, 1)
 
 spmu = spmu.join(pd.DataFrame(state_decile))
@@ -82,26 +93,25 @@ NB: These are all weighted by SPM unit, not individuals.
 """
 
 # TODO: Support multiple columns in mdf.weighted_sum, with a groupby.
-taxinc_state = spmu.groupby('state').apply(
-    lambda x: mdf.weighted_sum(x, 'taxinc', 'spmwt')
+taxinc_state = spmu.groupby("state").apply(
+    lambda x: mdf.weighted_sum(x, "taxinc", "spmwt")
 )
-taxinc_state.name = 'taxinc'
-children_state = spmu.groupby('state').apply(
-    lambda x: mdf.weighted_sum(x, 'child', 'spmwt')
+taxinc_state.name = "taxinc"
+children_state = spmu.groupby("state").apply(
+    lambda x: mdf.weighted_sum(x, "child", "spmwt")
 )
-children_state.name = 'children'
+children_state.name = "children"
 state = taxinc_state.to_frame().join(children_state)
-state['state_taxinc_per_child'] = state.taxinc / state.children
-spmu2 = spmu.merge(state[['state_taxinc_per_child']], left_on='state',
-                   right_index=True)
+state["state_taxinc_per_child"] = state.taxinc / state.children
+spmu2 = spmu.merge(state[["state_taxinc_per_child"]], left_on="state", right_index=True)
 
 """Calculate change per dollar of child allowance.
 
 NB: These are weighted by
 """
 
-total_taxinc = mdf.weighted_sum(spmu2, 'taxinc', 'spmwt')
-total_children = mdf.weighted_sum(spmu2, 'child', 'spmwt')
+total_taxinc = mdf.weighted_sum(spmu2, "taxinc", "spmwt")
+total_children = mdf.weighted_sum(spmu2, "child", "spmwt")
 fed_taxinc_per_child = total_taxinc / total_children
 fed_taxinc_per_child
 
@@ -115,23 +125,23 @@ pay that average amount, and any deviations from that will be in proportion
 to income.
 """
 
-spmu2['tax_per_dollar_ca_fed'] = spmu2.taxinc / fed_taxinc_per_child
-spmu2['net_per_dollar_ca_fed'] = spmu2.child - spmu2.tax_per_dollar_ca_fed
-spmu2['tax_per_dollar_ca_state'] = spmu2.taxinc / spmu2.state_taxinc_per_child
-spmu2['net_per_dollar_ca_state'] = spmu2.child - spmu2.tax_per_dollar_ca_state
-spmu2['tax_per_dollar_ca_deficit'] = 0
-spmu2['net_per_dollar_ca_deficit'] = spmu2.child
+spmu2["tax_per_dollar_ca_fed"] = spmu2.taxinc / fed_taxinc_per_child
+spmu2["net_per_dollar_ca_fed"] = spmu2.child - spmu2.tax_per_dollar_ca_fed
+spmu2["tax_per_dollar_ca_state"] = spmu2.taxinc / spmu2.state_taxinc_per_child
+spmu2["net_per_dollar_ca_state"] = spmu2.child - spmu2.tax_per_dollar_ca_state
+spmu2["tax_per_dollar_ca_deficit"] = 0
+spmu2["net_per_dollar_ca_deficit"] = spmu2.child
 
 
 # Check that it nets out, both overall and by decile.
-assert np.allclose(0,
-                   mdf.weighted_mean(spmu2, 'net_per_dollar_ca_fed', 'spmwt'))
+assert np.allclose(0, mdf.weighted_mean(spmu2, "net_per_dollar_ca_fed", "spmwt"))
 assert np.allclose(
     0,
-    spmu2.groupby('spm_resources_pp_decile').apply(
-        lambda x: mdf.weighted_mean(x, 'net_per_dollar_ca_fed',
-                                    'spmwt')).mean(),
-    atol=1e-5)
+    spmu2.groupby("spm_resources_pp_decile")
+    .apply(lambda x: mdf.weighted_mean(x, "net_per_dollar_ca_fed", "spmwt"))
+    .mean(),
+    atol=1e-5,
+)
 
 
 # Calculate data for each state x funding cross.
@@ -149,67 +159,76 @@ def decile_maker(funding: str, state: bool):
         * funding
     """
     # Use state decile for state-level calculation.
-    decile = 'spm_resources_pp_decile'
+    decile = "spm_resources_pp_decile"
     groupby = decile
     if state:
-        decile += '_state'
-        groupby = [decile, 'state']
+        decile += "_state"
+        groupby = [decile, "state"]
     # Set column for net change.
-    net = 'net_per_dollar_ca_' + funding
+    net = "net_per_dollar_ca_" + funding
     # Run grouped calculation.
-    res = spmu2.groupby(groupby).apply(
-        lambda x: mdf.weighted_mean(x, net, 'spmwt')).reset_index()
+    res = (
+        spmu2.groupby(groupby)
+        .apply(lambda x: mdf.weighted_mean(x, net, "spmwt"))
+        .reset_index()
+    )
     # Rename and set columns for returning.
-    res.rename({decile: 'decile', 0: 'net_per_dollar_ca'}, axis=1,
-               inplace=True)
-    res['funding'] = funding
+    res.rename({decile: "decile", 0: "net_per_dollar_ca"}, axis=1, inplace=True)
+    res["funding"] = funding
     if not state:
-        res['state'] = 'US'
+        res["state"] = "US"
     return res
 
 
-all_deciles = pd.concat([
-    decile_maker('deficit', True),
-    decile_maker('deficit', False),
-    decile_maker('fed', True),
-    decile_maker('fed', False),
-    decile_maker('state', True),
-    decile_maker('state', False)
-])
+all_deciles = pd.concat(
+    [
+        decile_maker("deficit", True),
+        decile_maker("deficit", False),
+        decile_maker("fed", True),
+        decile_maker("fed", False),
+        decile_maker("state", True),
+        decile_maker("state", False),
+    ]
+)
 
 
 # Calculate children for deficit-funded amount and
 # current resources for percentage differences.
 def avg_res(x):
-    resources = mdf.weighted_sum(x, 'spmtotres', 'spmwt')
+    resources = mdf.weighted_sum(x, "spmtotres", "spmwt")
     spmus = x.spmwt.sum()
     return resources / spmus
 
 
-state_decile_resources = spmu2.groupby(
-    ['spm_resources_pp_decile_state', 'state']).apply(avg_res).reset_index()
-state_decile_resources.rename({'spm_resources_pp_decile_state': 'decile'},
-                              axis=1, inplace=True)
-fed_decile_resources = spmu2.groupby(
-    'spm_resources_pp_decile').apply(avg_res).reset_index()
-fed_decile_resources.rename({'spm_resources_pp_decile': 'decile'},
-                            axis=1, inplace=True)
-fed_decile_resources['state'] = 'US'
+state_decile_resources = (
+    spmu2.groupby(["spm_resources_pp_decile_state", "state"])
+    .apply(avg_res)
+    .reset_index()
+)
+state_decile_resources.rename(
+    {"spm_resources_pp_decile_state": "decile"}, axis=1, inplace=True
+)
+fed_decile_resources = (
+    spmu2.groupby("spm_resources_pp_decile").apply(avg_res).reset_index()
+)
+fed_decile_resources.rename({"spm_resources_pp_decile": "decile"}, axis=1, inplace=True)
+fed_decile_resources["state"] = "US"
 decile_resources = pd.concat([state_decile_resources, fed_decile_resources])
-decile_resources.rename({0: 'current_resources'}, axis=1, inplace=True)
+decile_resources.rename({0: "current_resources"}, axis=1, inplace=True)
 
-all_deciles2 = all_deciles.merge(decile_resources, on=['decile', 'state'])
+all_deciles2 = all_deciles.merge(decile_resources, on=["decile", "state"])
 
 # Replicate for each monthly child allowance amount.
 l = []
 for i in np.arange(0, 501, 25):
     tmp = all_deciles2.copy(deep=True)
-    tmp['monthly_ca'] = i
-    tmp['net_chg'] = i * 12 * tmp.net_per_dollar_ca
+    tmp["monthly_ca"] = i
+    tmp["net_chg"] = i * 12 * tmp.net_per_dollar_ca
     l.append(tmp)
 ca_state_decile = pd.concat(l)
 
-ca_state_decile['pct_chg'] = (
-    100 * ca_state_decile.net_chg / ca_state_decile.current_resources)
+ca_state_decile["pct_chg"] = (
+    100 * ca_state_decile.net_chg / ca_state_decile.current_resources
+)
 
-ca_state_decile.to_csv('deciles.csv', index=False)
+ca_state_decile.to_csv("deciles.csv", index=False)
