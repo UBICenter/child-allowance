aaccbec51a82b7df2e58c9d6b309d8798a442d6b
John Walker <67374091+jwal1024@users.noreply.github.com>
2020-12-08 11:55:24 +1100

    Race categories, prelim results

--- jb/child_allowance.py
+++ jb/child_allowance.py
@@ -6,10 +6,7 @@
 ## % change in poverty/child poverty.
 ## In which states do childcare expenses push the most kids/adults into poverty.
 
-# 1. Calculate largest differences in the poverty rate by simulation by state.
-# 2. Do this in a second commit: Construct function to turn race into these 4 buckets:
-# BLACK HISPANIC White-non-hispanic other-non-hispanic
-##### CHANGES TO THE RACE DELINIATIONS?
+# "wsl$\\Ubuntu\\home\\johnwalker\\childallowance\\jb\\datacps_00003.csv.gz",
 
 # Preamble and read data
 import microdf as mdf
@@ -17,9 +14,10 @@ import pandas as pd
 import numpy as np
 import us
 
+
 # Read in census data and specify columns for use
 raw = pd.read_csv(
-    "wsl$\\Ubuntu\\home\\johnwalker\\childallowance\\jb\\datacps_00003.csv.gz",
+    "C:\\Users\\John Walker\\Downloads\\cps_00003.csv.gz",
     usecols=[
         "YEAR",
         "MONTH",
@@ -52,15 +50,62 @@ person_1["preschool"] = person_1.age.between(3, 5)
 person_1["person"] = 1
 
 # Redefine race categories
-person_1.race = person_1.race.replace(
-    {
-        100: "White",
-        200: "Black",
-        300: "American First Nations",
-        651: "Asian",
-        651: "Hawaiian/Pacific Islander",
-    }
+person_1["race"].mask(
+    person_1.race.between(200, 999, inclusive=False), "Other", inplace=True
 )
+person_1["race"].mask(person_1.race == 100, "White", inplace=True)
+person_1["race"].mask(person_1.race == 200, "Black", inplace=True)
+person_1["race"].mask(person_1.race == 999, "Race unknown", inplace=True)
+
+# Redefine hispanic categories
+person_1["hispan"].mask(
+    person_1.hispan.between(100, 612, inclusive=True), "Hispanic", inplace=True
+)
+person_1["hispan"].mask(person_1.hispan == 0, "Not Hispanic", inplace=True)
+person_1["hispan"].mask(
+    (person_1.hispan != "Hispanic") & (person_1.hispan != "Not Hispanic"),
+    "Hispanic status unknown",
+    inplace=True,
+)
+
+# Combine race + hispanic categories
+person_1["race_hispan"] = np.nan
+person_1["race_hispan"] = person_1["race_hispan"].mask(
+    (person_1["race"] == "White") & (person_1["hispan"] == "Not Hispanic"),
+    "White non-Hispanic",
+)
+person_1["race_hispan"] = person_1["race_hispan"].mask(
+    (person_1["race"] == "Black") & (person_1["hispan"] == "Not Hispanic"),
+    "Black",
+)
+person_1["race_hispan"] = person_1["race_hispan"].mask(
+    (person_1["race"] == "Other") & (person_1["hispan"] == "Not Hispanic"),
+    "Other non-Hispanic",
+)
+person_1["race_hispan"] = person_1["race_hispan"].mask(
+    person_1["hispan"] == "Hispanic", "Hispanic"
+)
+person_1["race_hispan"] = person_1["race_hispan"].mask(
+    (person_1["race"] == "Race unknown") & (["hispan"] == "Not Hispanic"),
+    "Race unknown",
+)
+person_1["race_hispan"] = person_1["race_hispan"].mask(
+    (person_1["race"] == "Race unknown")
+    & (["hispan"] == "Hispanic status unknown"),
+    "Race unknown",
+)
+
+# Relabel sex categories
+person_1["sex"].mask(person_1["sex"] == 1, "Male", inplace=True)
+person_1["sex"].mask(person_1["sex"] == 2, "Female", inplace=True)
+
+## TO DO
+# Create State categories
+# person_1["statefip"] = (
+#  pd.Series(state.index)
+#  .apply(lambda x: us.states.lookup(str(x).zfill(2)).name)
+#  .tolist()
+# )
 
 # Define data collected at the SPM unit level
 spmu_cols = [
@@ -124,13 +169,8 @@ poverty_rate_sex = (
     .apply(lambda x: mdf.weighted_mean(x, "poverty_flag", "asecwt"))
     .reset_index()
 )
-poverty_rate_race = (
-    person.groupby(["sim_flag", "race"])
-    .apply(lambda x: mdf.weighted_mean(x, "poverty_flag", "asecwt"))
-    .reset_index()
-)
-poverty_rate_hispan = (
-    person.groupby(["sim_flag", "hispan"])
+poverty_rate_race_hispan = (
+    person.groupby(["sim_flag", "race_hispan"])
     .apply(lambda x: mdf.weighted_mean(x, "poverty_flag", "asecwt"))
     .reset_index()
 )
@@ -139,8 +179,27 @@ poverty_rate_state = (
     .apply(lambda x: mdf.weighted_mean(x, "poverty_flag", "asecwt"))
     .reset_index()
 )
+
+# Rename constructed poverty_rate
 poverty_rate.rename({0: "poverty_rate"}, axis=1, inplace=True)
 poverty_rate_sex.rename({0: "poverty_rate"}, axis=1, inplace=True)
-poverty_rate_race.rename({0: "poverty_rate"}, axis=1, inplace=True)
-poverty_rate_hispan.rename({0: "poverty_rate"}, axis=1, inplace=True)
+poverty_rate_race_hispan.rename({0: "poverty_rate"}, axis=1, inplace=True)
 poverty_rate_state.rename({0: "poverty_rate"}, axis=1, inplace=True)
+
+# Construct first differences (pp changes)
+# dif_pov_flat = poverty_rate.loc[poverty_rate["sim_flag"] == 1] -
+# # poverty_rate.loc[poverty_rate["simflag"] == 0]
+# dif_pov_flat_sex =
+# dif_pov_replace = poverty_rate["sim_flag"] == 2 - poverty_rate["simflag"] == 0
+# dif_pov_replace_sex =
+
+# Interesting findings
+# The poverty change is much larger for female-identifying people and generally
+# flat transfer is much more effective, particularly for female-identifying.
+# The poverty change for the flat transfer is largest for Black and \
+# Hispanic populations ~3%, lower for White ~1.2%, and other non-hispanic ~1.8%.
+
+
+# Sort by poverty rate to show max sizes (state)
+# poverty_rate_state = poverty_rate_state.sort_values(by="", ascending=False)
+# TO DO: Insert column value.
