2f6685529b1e802b2cc5096944b0e9b091ff26bd
John Walker <67374091+jwal1024@users.noreply.github.com>
2020-12-08 21:15:21 +1100

    Gini coefficients, child poverty, states.
    Code put in for gini coefficients, child poverty, state labels. Dataframes renamed for clarity. Notes at beginning and end clarified.  May be one more commit to meet PEP8 code guidelines.

--- jb/child_allowance.py
+++ jb/child_allowance.py
@@ -1,12 +1,7 @@
-## Calculate largest differences in the poverty rate by simulation by state.
-## Interested in child poverty as well.
-## How much does child allowance compare to child care
-
-## Interested in WHERE the poverty differential is taking place expenditure-wise
-## % change in poverty/child poverty.
-## In which states do childcare expenses push the most kids/adults into poverty.
-
-# "wsl$\\Ubuntu\\home\\johnwalker\\childallowance\\jb\\datacps_00003.csv.gz",
+# Roadmap:
+# Comparison to other potential policies
+# Replicate plotly code from the child allowance for JB
+# Move to second simulation
 
 # Preamble and read data
 import microdf as mdf
@@ -14,10 +9,9 @@ import pandas as pd
 import numpy as np
 import us
 
-
 # Read in census data and specify columns for use
 raw = pd.read_csv(
-    "C:\\Users\\John Walker\\Downloads\\cps_00003.csv.gz",
+    "https://github.com/UBICenter/child-allowance/blob/master/jb/data/cps_00003.csv.gz",
     usecols=[
         "YEAR",
         "MONTH",
@@ -38,74 +32,69 @@ raw = pd.read_csv(
 
 # Create a copy of the raw dataset and make column names non-capitalized
 # for readability
-person_1 = raw.copy(deep=True)
-person_1 = person_1[~person_1.ASECWT.isnull()]
-person_1.columns = person_1.columns.str.lower()
+person = raw.copy(deep=True)
+person = person[~person.ASECWT.isnull()]
+person.columns = person.columns.str.lower()
+
+# Asec weights are year-person units, and we average over 3 years,
+# so divide by the 3 years to give per year weights.
+person.asecwt /= 3
 
 # Define child age identifiers
-person_1["child_6"] = person_1.age < 6
-person_1["infant"] = person_1.age < 1
-person_1["toddler"] = person_1.age.between(1, 2)
-person_1["preschool"] = person_1.age.between(3, 5)
-person_1["person"] = 1
+person["child_6"] = person.age < 6
+person["infant"] = person.age < 1
+person["toddler"] = person.age.between(1, 2)
+person["preschool"] = person.age.between(3, 5)
+person["person"] = 1
 
 # Redefine race categories
-person_1["race"].mask(
-    person_1.race.between(200, 999, inclusive=False), "Other", inplace=True
+person.race.mask(
+    person.race.between(200, 999, inclusive=False), "Other", inplace=True
 )
-person_1["race"].mask(person_1.race == 100, "White", inplace=True)
-person_1["race"].mask(person_1.race == 200, "Black", inplace=True)
-person_1["race"].mask(person_1.race == 999, "Race unknown", inplace=True)
+person.race.mask(person.race == 100, "White", inplace=True)
+person.race.mask(person.race == 200, "Black", inplace=True)
+person.race.mask(person.race == 999, "Race unknown", inplace=True)
 
 # Redefine hispanic categories
-person_1["hispan"].mask(
-    person_1.hispan.between(100, 612, inclusive=True), "Hispanic", inplace=True
+person.hispan.mask(
+    person.hispan.between(100, 612, inclusive=True), "Hispanic", inplace=True
 )
-person_1["hispan"].mask(person_1.hispan == 0, "Not Hispanic", inplace=True)
-person_1["hispan"].mask(
-    (person_1.hispan != "Hispanic") & (person_1.hispan != "Not Hispanic"),
+person.hispan.mask(person.hispan == 0, "Not Hispanic", inplace=True)
+person.hispan.mask(
+    (person.hispan != "Hispanic") & (person.hispan != "Not Hispanic"),
     "Hispanic status unknown",
     inplace=True,
 )
 
 # Combine race + hispanic categories
-person_1["race_hispan"] = np.nan
-person_1["race_hispan"] = person_1["race_hispan"].mask(
-    (person_1["race"] == "White") & (person_1["hispan"] == "Not Hispanic"),
+person["race_hispan"] = "Other or Unknown"
+person.race_hispan.mask(
+    (person.race == "White") & (person.hispan == "Not Hispanic"),
     "White non-Hispanic",
+    inplace=True,
 )
-person_1["race_hispan"] = person_1["race_hispan"].mask(
-    (person_1["race"] == "Black") & (person_1["hispan"] == "Not Hispanic"),
+person.race_hispan.mask(
+    (person.race == "Black") & (person.hispan == "Not Hispanic"),
     "Black",
+    inplace=True,
 )
-person_1["race_hispan"] = person_1["race_hispan"].mask(
-    (person_1["race"] == "Other") & (person_1["hispan"] == "Not Hispanic"),
+person.race_hispan.mask(
+    (person.race == "Other") & (person.hispan == "Not Hispanic"),
     "Other non-Hispanic",
+    inplace=True,
 )
-person_1["race_hispan"] = person_1["race_hispan"].mask(
-    person_1["hispan"] == "Hispanic", "Hispanic"
-)
-person_1["race_hispan"] = person_1["race_hispan"].mask(
-    (person_1["race"] == "Race unknown") & (["hispan"] == "Not Hispanic"),
-    "Race unknown",
-)
-person_1["race_hispan"] = person_1["race_hispan"].mask(
-    (person_1["race"] == "Race unknown")
-    & (["hispan"] == "Hispanic status unknown"),
-    "Race unknown",
-)
+person.race_hispan.mask(person.hispan == "Hispanic", "Hispanic", inplace=True)
 
 # Relabel sex categories
-person_1["sex"].mask(person_1["sex"] == 1, "Male", inplace=True)
-person_1["sex"].mask(person_1["sex"] == 2, "Female", inplace=True)
+person["sex"].mask(person["sex"] == 1, "Male", inplace=True)
+person["sex"].mask(person["sex"] == 2, "Female", inplace=True)
 
-## TO DO
 # Create State categories
-# person_1["statefip"] = (
-#  pd.Series(state.index)
-#  .apply(lambda x: us.states.lookup(str(x).zfill(2)).name)
-#  .tolist()
-# )
+person["state"] = (
+    pd.Series(person.statefip)
+    .apply(lambda x: us.states.lookup(str(x).zfill(2)).name)
+    .tolist()
+)
 
 # Define data collected at the SPM unit level
 spmu_cols = [
@@ -119,7 +108,7 @@ spmu_cols = [
 ]
 
 spmu = pd.DataFrame(
-    person_1.groupby(spmu_cols)[
+    person.groupby(spmu_cols)[
         ["child_6", "infant", "toddler", "preschool", "person"]
     ].sum()
 ).reset_index()
@@ -134,9 +123,9 @@ spmu_flat_transfer = spmu.copy(deep=True)
 
 # Generate simulation-level flags to separate datasets,
 # 0 = base case, 1 = cost replacement design, 2 = flat transfer
-spmu["sim_flag"] = 0
-spmu_replace_cost["sim_flag"] = 1
-spmu_flat_transfer["sim_flag"] = 2
+spmu["sim_flag"] = "baseline"
+spmu_replace_cost["sim_flag"] = "cc_replacement"
+spmu_flat_transfer["sim_flag"] = "child_allowance"
 
 # Caluclate new income by simulation
 spmu_replace_cost.spmftotval += spmu_replace_cost.spmchxpns
@@ -151,31 +140,52 @@ spmu_sim = pd.concat(
 
 # Create poverty flags on simulated incomes
 spmu_sim["poverty_flag"] = spmu_sim.spmftotval < spmu_sim.spmthresh
+spmu_sim["poverty_flag_child"] = (spmu_sim.spmftotval < spmu_sim.spmthresh) & (
+    spmu_sim.child_6 == 1
+)
+
+# Calculate per person spmftotval (resources)
+spmu_sim["resources_pp"] = spmu_sim.spmftotval / spmu_sim.person
 
 # Construct dataframe to disaggregate poverty flag to person level
-person = person_1.merge(
-    spmu_sim[["spmfamunit", "poverty_flag", "sim_flag", "year"]],
+person_sim = person.drop("spmftotval", axis=1).merge(
+    spmu_sim[
+        [
+            "spmfamunit",
+            "poverty_flag",
+            "poverty_flag_child",
+            "sim_flag",
+            "year",
+            "spmftotval",
+            "resources_pp",
+        ]
+    ],
     on=["spmfamunit", "year"],
 )
 
 # Consider sex, race, state heterogeneity
 poverty_rate = (
-    person.groupby(["sim_flag"])
+    person_sim.groupby(["sim_flag"])
     .apply(lambda x: mdf.weighted_mean(x, "poverty_flag", "asecwt"))
     .reset_index()
 )
+poverty_rate_child = (
+    person_sim.groupby(["sim_flag"])
+    .apply(lambda x: mdf.weighted_mean(x, "poverty_flag_child", "asecwt"))
+    .reset_index()
+)
 poverty_rate_sex = (
-    person.groupby(["sim_flag", "sex"])
+    person_sim.groupby(["sim_flag", "sex"])
     .apply(lambda x: mdf.weighted_mean(x, "poverty_flag", "asecwt"))
     .reset_index()
 )
 poverty_rate_race_hispan = (
-    person.groupby(["sim_flag", "race_hispan"])
+    person_sim.groupby(["sim_flag", "race_hispan"])
     .apply(lambda x: mdf.weighted_mean(x, "poverty_flag", "asecwt"))
     .reset_index()
 )
 poverty_rate_state = (
-    person.groupby(["sim_flag", "statefip"])
+    person_sim.groupby(["sim_flag", "state"])
     .apply(lambda x: mdf.weighted_mean(x, "poverty_flag", "asecwt"))
     .reset_index()
 )
@@ -186,20 +196,39 @@ poverty_rate_sex.rename({0: "poverty_rate"}, axis=1, inplace=True)
 poverty_rate_race_hispan.rename({0: "poverty_rate"}, axis=1, inplace=True)
 poverty_rate_state.rename({0: "poverty_rate"}, axis=1, inplace=True)
 
-# Construct first differences (pp changes)
-# dif_pov_flat = poverty_rate.loc[poverty_rate["sim_flag"] == 1] -
-# # poverty_rate.loc[poverty_rate["simflag"] == 0]
-# dif_pov_flat_sex =
-# dif_pov_replace = poverty_rate["sim_flag"] == 2 - poverty_rate["simflag"] == 0
-# dif_pov_replace_sex =
+# Construct poverty percentage changes
+state = poverty_rate_state.pivot_table(
+    values="poverty_rate", index="state", columns="sim_flag"
+)
 
-# Interesting findings
-# The poverty change is much larger for female-identifying people and generally
-# flat transfer is much more effective, particularly for female-identifying.
-# The poverty change for the flat transfer is largest for Black and \
+
+def percent_change(base, new):
+    return 100 * (new - base) / new
+
+
+state["poverty_change_cc"] = percent_change(
+    state.baseline, state.cc_replacement
+)
+state["poverty_change_flat"] = percent_change(
+    state.baseline, state.child_allowance
+)
+
+# Gini coefficients
+mdf.gini(person_sim, "spmftotval", "asecwt")
+mdf.gini(person_sim, "resources_pp", "asecwt")
+person_sim.groupby("sim_flag").apply(
+    lambda x: mdf.gini(x, "spmftotval", "asecwt")
+)
+person_sim.groupby("sim_flag").apply(
+    lambda x: mdf.gini(x, "resources_pp", "asecwt")
+)
+
+# Re-arrange by poverty rate
+state.sort_values(by="poverty_change_flat", ascending=False)
+
+# Interesting findings:
+# Flat transfer: Roughly 10* the impact on poverty and gini coefficient
+# compared to the childcare provision equivalent policy (paying costs)
+# The poverty change is much larger for female-identifying people.
+# The poverty change for the flat transfer is largest for Black and
 # Hispanic populations ~3%, lower for White ~1.2%, and other non-hispanic ~1.8%.
-
-
-# Sort by poverty rate to show max sizes (state)
-# poverty_rate_state = poverty_rate_state.sort_values(by="", ascending=False)
-# TO DO: Insert column value.
